10e356115d86869687a28e9416e9cbb8cf5eb633
v_kailertb <kailertb@gmail.com>
2021-02-22 14:13:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -261,7 +261,7 @@ addClient
 
 # The MIT License (MIT)
 
-# Copyright (c) 2020 Rajan Patel
+# Copyright (c) 2021 Kailert Bunlert
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
